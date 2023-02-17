4f11f3541da312970a2ea59ed12c6ba2d777356c
sydneyyyyy <84032109+sydneyyyyy@users.noreply.github.com>
2023-02-17 21:18:19 +0000

    added for loop

--- README.md
+++ README.md
@@ -12,3 +12,64 @@
 
 You can nest ternary operators however, that could prevent code readability. 
 
+### Statement vs. Expression
+
+Expression is something that returns a value. An expression is also a statement but for example an if statement is just a statement because it does not yield a value. 
+
+### Boolean Tricks w/ Logical Operators
+
+* `!!` -> Boolean Coercion, whereas `!` is the not operator the `!!` is the opposite of that (e.g. !!"", !!1). 
+* `||` -> Default value assignment via OR operator (e.g. const name = someInput || 'Max') returns the first truthy value without converting to a real boolean.
+* `&&` -> Use value if condition is true via AND Operator (e.g. const name = isLoggedIn && 'Max') 'Max' is set if isLoggedIn is true, returns false otherwise. 
+
+```
+const enteredValue = '';
+const userName = enteredValue && 'Anna';
+```
+
+If the first operand is falsy, && always returns the first operand, NEVER the second one. And if the first operand is TRUTHY, the AND operator ALWAYS returns the second value. Even if that operand should be falsy. 
+
+### Switch Statement
+
+* Break Keyword MUST be used at the end of each case. Tells JS that once that case has been handled/executed, then no other case should be handled/executed. 
+
+Switch Statements fall through automatically execute every statement until the break keyword. 
+
+### Loops
+
+Loops are used when you need to execute code multiple times. 
+
+* For Loop -> execute code a certain amount of times (with counter variable).
+
+```
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
+```
+
+* For-of Loop -> execute for every element in an array
+
+```
+for (const el of array) {
+    console.log(el);
+}
+```
+
+* For-in Loop - execute for every key in an object
+
+```
+for (const key in object) {
+    console.log(key);
+    console.log(obj[key]);
+}
+```
+
+* While Loop -> execute code as long as a condition is true
+
+```
+while (isLoggedIn) {
+    ...
+}
+```
+
+
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -34,41 +34,86 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
         finalMonsterHealth: monsterHealth,
         finalPlayerHealth: playerHealth
     };
-    if (event === LOG_EVENT_PLAYER_ATTACK) {
-        // can alternatively set the property dynamically rather than repeating the object. 
-        logEntry.target = "MONSTER";
-    } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-        logEntry = {
-            event: event,
-            value: value,
-            target: 'MONSTER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
-    } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-        logEntry = {
-            event: event,
-            value: value,
-            target: 'PLAYER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
-    } else if (event === LOG_EVENT_PLAYER_HEAL) {
-        logEntry = {
-            event: event,
-            value: value,
-            target: 'PLAYER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
-    } else if (event === LOG_EVENT_GAME_OVER) {
-        logEntry = {
-            event: event,
-            value: value,
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
+
+    switch (event) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'MONSTER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            }
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };
+            break;
+        case LOG_EVENT_GAME_OVER:
+            logEntry = {
+                event: event,
+                value: value,
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };
+            break;
+        default:
+            logEntry = {};
     }
+
+    // if (event === LOG_EVENT_PLAYER_ATTACK) {
+    //     // can alternatively set the property dynamically rather than repeating the object. 
+    //     logEntry.target = "MONSTER";
+    // } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         target: 'MONSTER',
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     };
+    // } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         target: 'PLAYER',
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     };
+    // } else if (event === LOG_EVENT_PLAYER_HEAL) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         target: 'PLAYER',
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     };
+    // } else if (event === LOG_EVENT_GAME_OVER) {
+    //     logEntry = {
+    //         event: event,
+    //         value: value,
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     };
+    // }
+
     battleLog.push(logEntry);
 }
 
@@ -189,6 +234,14 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
+    for (let i = 0; i < 3; i++) {
+        console.log('-----------------');
+    }
+
+    for (let i = 10; i > 0; i--) {
+        console.log(i);
+    }
+    
     console.log(battleLog);
 }
 
